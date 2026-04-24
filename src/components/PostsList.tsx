6aa9b413ad1956817b2750c62793d89d321bab5b
Alexandr <AlexandrKsenz@gmail.com>
2026-04-24 17:21:33 +0200

    solution

--- src/components/PostsList.tsx
+++ src/components/PostsList.tsx
@@ -1,86 +1,74 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { PostsContext } from '../store/PostsProvider';
+import { getComments } from '../api';
+import { Post } from '../types/Post';
+import { PostContext } from '../store/PostProvider';
+import { CommentsContext } from '../store/CommentsProvider';
+import classNames from 'classnames';
 
-export const PostsList: React.FC = () => (
-  <div data-cy="PostsList">
-    <p className="title">Posts:</p>
+export const PostsList: React.FC = () => {
+  const { posts } = useContext(PostsContext);
+  const { post, setPost } = useContext(PostContext);
+  const { setComments, setCommentsStatus } = useContext(CommentsContext);
 
-    <table className="table is-fullwidth is-striped is-hoverable is-narrow">
-      <thead>
-        <tr className="has-background-link-light">
-          <th>#</th>
-          <th>Title</th>
-          {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-          <th> </th>
-        </tr>
-      </thead>
+  const openPost = async (currentPost: Post) => {
+    setComments([]);
+    setCommentsStatus('loading');
+    setPost(currentPost);
 
-      <tbody>
-        <tr data-cy="Post">
-          <td data-cy="PostId">17</td>
+    try {
+      const response = await getComments(currentPost.id);
 
-          <td data-cy="PostTitle">
-            fugit voluptas sed molestias voluptatem provident
-          </td>
+      if (!response) {
+        throw new Error('404');
+      }
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
+      setComments(response);
+      setCommentsStatus('success');
+    } catch {
+      setCommentsStatus('error');
+    }
+  };
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">18</td>
+  return (
+    <div data-cy="PostsList">
+      <p className="title">Posts:</p>
 
-          <td data-cy="PostTitle">
-            voluptate et itaque vero tempora molestiae
-          </td>
+      <table className="table is-fullwidth is-striped is-hoverable is-narrow">
+        <thead>
+          <tr className="has-background-link-light">
+            <th>#</th>
+            <th>Title</th>
+            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
+            <th> </th>
+          </tr>
+        </thead>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link"
-            >
-              Close
-            </button>
-          </td>
-        </tr>
+        <tbody>
+          {posts.map(item => {
+            return (
+              <tr key={item.id} data-cy="Post">
+                <td data-cy="PostId">{item.id}</td>
 
-        <tr data-cy="Post">
-          <td data-cy="PostId">19</td>
-          <td data-cy="PostTitle">adipisci placeat illum aut reiciendis qui</td>
+                <td data-cy="PostTitle">{item.title}</td>
 
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-
-        <tr data-cy="Post">
-          <td data-cy="PostId">20</td>
-          <td data-cy="PostTitle">doloribus ad provident suscipit at</td>
-
-          <td className="has-text-right is-vcentered">
-            <button
-              type="button"
-              data-cy="PostButton"
-              className="button is-link is-light"
-            >
-              Open
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-);
+                <td className="has-text-right is-vcentered">
+                  <button
+                    type="button"
+                    data-cy="PostButton"
+                    className={classNames('button is-link', {
+                      ['is-light']: post?.id !== item.id,
+                    })}
+                    onClick={() => openPost(item)}
+                  >
+                    {item.id === post?.id ? 'Close' : 'Open'}
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+};
